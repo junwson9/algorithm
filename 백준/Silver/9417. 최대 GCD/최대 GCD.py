834015eed49711117eb4cc8d8dba41ba88326b0a
JW <119388872+junwson9@users.noreply.github.com>
2024-01-04 21:06:05 +0900

    [Silver IV] Title: 최대 GCD, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/9417. 최대 GCD/최대 GCD.py
+++ 백준/Silver/9417. 최대 GCD/최대 GCD.py
@@ -1,16 +1,18 @@
 import sys
-import math
+from itertools import combinations
 input = sys.stdin.readline
-N = int(input())
-for _ in range(N):
+T = int(input())
+for _ in range(T):
     lst = list(map(int, input().split()))
-    mx = 1
-    for i in range(len(lst)-1):
-        for j in range(i+1,len(lst)):
-            tmp = math.gcd(lst[i],lst[j])
-            if tmp > mx:
-                mx = tmp
-    print(mx)
+    def gcd(a,b):
+        while b > 0:
+            a,b = b, a%b
+        return a
 
+    tmp = list(combinations(lst,2))
+    ans = 1
+    for i in range(len(tmp)):
+        if gcd(tmp[i][0],tmp[i][1]) > ans:
+            ans = gcd(tmp[i][0],tmp[i][1])
 
-
+    print(ans)
