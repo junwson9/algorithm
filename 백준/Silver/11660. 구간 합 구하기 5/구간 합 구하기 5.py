dc21d4469ed75a8e962d8c79d4231adff8803d51
JW <119388872+junwson9@users.noreply.github.com>
2024-01-11 20:10:11 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 1168 ms, Memory: 105940 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
@@ -1,15 +1,12 @@
 import sys
 input = sys.stdin.readline
-N,M = map(int, input().split())
-arr = [list(map(int ,input().split())) for _ in range(N)]
-csum = [[0]*(N+1) for _ in range(N+1)]
-for i in range(1,N+1):
-    for j in range(1,N+1):
-        csum[i][j] = csum[i-1][j]+csum[i][j-1]-csum[i-1][j-1]+arr[i-1][j-1]
-
-for _ in range(M):
+n, m = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(n)]
+prefix_sum = [[0]*(n+1) for _ in range(n+1)]
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        prefix_sum[i][j] = prefix_sum[i-1][j]+prefix_sum[i][j-1]-prefix_sum[i-1][j-1]+arr[i-1][j-1]
+for _ in range(m):
     x1,y1,x2,y2 = map(int,input().split())
-    result = csum[x2][y2]-csum[x2][y1-1]-csum[x1-1][y2]+csum[x1-1][y1-1]
+    result = prefix_sum[x2][y2] - prefix_sum[x2][y1-1] - prefix_sum[x1-1][y2] + prefix_sum[x1-1][y1-1]
     print(result)
-
-
