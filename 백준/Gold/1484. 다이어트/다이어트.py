a82a0cfac0a0f0da9a25cf4042611efa8fe0e254
JW <119388872+junwson9@users.noreply.github.com>
2024-01-18 18:12:36 +0900

    [Gold V] Title: 다이어트, Time: 72 ms, Memory: 31388 KB -BaekjoonHub

--- 백준/Gold/1484. 다이어트/다이어트.py
+++ 백준/Gold/1484. 다이어트/다이어트.py
@@ -1,27 +1,20 @@
 import sys
-input = sys.stdin.readline
-G = int(input())
-s = 2
-e = 1
-ans = []
+
+g = int(sys.stdin.readline().rstrip())
+
+left, right = 1, 1
+result = []
+
 while True:
-    if s+e > G:
-        if len(ans) == 0:
-            ans.append(-1)
-        break
-    if e >= s:
-        if len(ans) == 0:
-            ans.append(-1)
-        break
-    tmp = (s+e)*(s-e)
-    if tmp < G:
-        s += 1
-        e = 1
-    elif tmp > G:
-        e += 1
+    diff = right**2 - left**2
+    if right - left == 1 and diff > g: break
+    # 제곱되는 right, left 수 차가 1이고 제곱수의 차가 g보다 크면 더 이상 g를 구할 수 없다.
+
+    if diff > g: left += 1
+    elif diff < g: right +=1
     else:
-        ans.append(s)
-        s += 1
-        e = 1
-for n in ans:
-    print(n)
+        result.append(right)
+        right += 1
+
+if result: print(*result, sep='\n')
+else: print(-1)
