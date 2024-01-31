3706f7e1825a5f9034eee5c362cd55efd23a4f1f
JW <119388872+junwson9@users.noreply.github.com>
2024-01-31 20:07:21 +0900

    [Gold V] Title: 암호 만들기, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.py
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.py
@@ -1,26 +1,26 @@
-def dfs(n,tmp,idx):
+import sys
+input = sys.stdin.readline
+L,C = map(int, input().split())
+lst = list(input().split())
+lst.sort()
+tmp = []
+def dfs(n,idx):
     if n == L:
         cnt1 = 0
         cnt2 = 0
-        for s in tmp:
-            if s == 'a' or s =='e' or s=='i' or s=='o' or s=='u':
+        for i in range(L):
+            if tmp[i] == 'a' or tmp[i] == 'e' or tmp[i] == 'i' or tmp[i] == 'o' or tmp[i] == 'u':
                 cnt1 += 1
             else:
                 cnt2 += 1
         if cnt1 >= 1 and cnt2 >= 2:
-            rlt.append(tmp)
-            return
+            rlt = ''.join(map(str,tmp))
+            print(rlt)
+        return
 
     for i in range(idx,C):
-        dfs(n+1, tmp+[lst[i]], i+1)
+        tmp.append(lst[i])
+        dfs(n+1,i+1)
+        tmp.pop()
 
-L, C = map(int, input().split())
-lst = list(input().split())
-lst.sort()
-tmp = []
-arr = []
-rlt = []
-v = [0]*len(lst)
-dfs(0,tmp,0)
-for pwd in rlt:
-    print(''.join(pwd))
+dfs(0,0)
