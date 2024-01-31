f17dd58f0cef47381c3ba1cb5b982257219b5af7
JW <119388872+junwson9@users.noreply.github.com>
2024-01-31 19:19:27 +0900

    [Silver I] Title: 부등호, Time: 176 ms, Memory: 112240 KB -BaekjoonHub

--- 백준/Silver/2529. 부등호/부등호.py
+++ 백준/Silver/2529. 부등호/부등호.py
@@ -1,46 +1,41 @@
+import sys
+input = sys.stdin.readline
+k = int(input())
+lst = list(map(str,input().split()))
+v = [0]*(10)
+num = [0,1,2,3,4,5,6,7,8,9]
+tmp = []
+mx = '0'
+mn = '9'*10
 def dfs(n):
-    global min_ans, max_ans
-
+    global mx,mn
     if n >= 2:
         if lst[n-2] == '<':
             if tmp[n-2] > tmp[n-1]:
                 return
+
         if lst[n-2] == '>':
             if tmp[n-2] < tmp[n-1]:
                 return
 
-    if n == N:
-        for c in range(k):
-            if lst[c] == '<':
-                if tmp[c] > tmp[c+1]:
-                    return
-            if lst[c] == '>':
-                if tmp[c] < tmp[c+1]:
-                    return
-        else:
-            rlt = ''.join(map(str,tmp))
-            if int(min_ans) > int(rlt):
-                min_ans = rlt
-            if int(max_ans) < int(rlt):
-                max_ans = rlt
-            return
 
+    if n == k+1:
+        rlt = ''.join(map(str,tmp))
+        if rlt > mx:
+            mx = rlt
+        if rlt < mn:
+            mn = rlt
+        return
 
     for i in range(10):
         if v[i] == 0:
             v[i] = 1
             tmp.append(i)
             dfs(n+1)
-            tmp.pop()
             v[i] = 0
+            tmp.pop()
 
-k = int(input())
-lst = list(input().split())
-tmp = []
-min_ans = '9'*(k+1)
-max_ans = '0'
-v = [0]*10
-N = k+1
+
 dfs(0)
-print(max_ans)
-print(min_ans)
+print(mx)
+print(mn)
