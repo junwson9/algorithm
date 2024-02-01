69dbcab96f3666e027d8c7c488875c493551ad2a
JW <119388872+junwson9@users.noreply.github.com>
2024-02-01 23:53:37 +0900

    [Gold V] Title: 치킨 배달, Time: 200 ms, Memory: 113196 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.py
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.py
@@ -1,36 +1,39 @@
 import sys
-def dfs(n,i):
-    global ans
-    if n == M:
-        rlt = 0
-        for hi,hj in home:
-            dist = 2*N+1
-            for ci,cj in select:
-                dist = min(dist,abs(ci-hi)+abs(cj-hj))
-            rlt += dist
-        if rlt < ans:
-            ans = rlt
-        return
-    for idx in range(i,K):
-        select.append(chicken[idx])
-        dfs(n+1,idx+1)
-        select.pop()
-
-
 input = sys.stdin.readline
 N,M = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(N)]
+chk = []
 home = []
-chicken = []
 for i in range(N):
     for j in range(N):
+        if arr[i][j] == 2:
+            chk.append((i,j))
         if arr[i][j] == 1:
             home.append((i,j))
-        if arr[i][j] == 2:
-            chicken.append((i,j))
-K = len(chicken)
-ans = 2*N*len(home)+1
-select = []
-for t in range(K):
-    dfs(0,t)
+
+
+tmp = []
+def check(tmp):
+    sm = 0
+    for i,j in home:
+        mn = N*N
+        for ci,cj in tmp:
+            if abs(i-ci)+abs(j-cj) < mn:
+                mn = abs(i-ci)+abs(j-cj)
+        sm += mn
+    return sm
+
+ans = 1e9
+def dfs(n,idx):
+    global ans
+    if n == M:
+        if check(tmp) < ans:
+            ans = check(tmp)
+        return
+
+    for i in range(idx,len(chk)):
+        tmp.append(chk[i])
+        dfs(n+1,i+1)
+        tmp.pop()
+dfs(0,0)
 print(ans)
