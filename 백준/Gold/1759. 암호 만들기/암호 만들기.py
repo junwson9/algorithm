a9d22e8f2afc30466946fa848ac48bc72a3db498
JW <119388872+junwson9@users.noreply.github.com>
2024-02-07 11:00:32 +0900

    [Gold V] Title: 암호 만들기, Time: 144 ms, Memory: 110972 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.py
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.py
@@ -3,24 +3,19 @@ input = sys.stdin.readline
 L,C = map(int, input().split())
 lst = list(input().split())
 lst.sort()
-tmp = []
-def dfs(n,idx):
+def dfs(n,idx,rlt):
     if n == L:
         cnt1 = 0
         cnt2 = 0
-        for i in range(L):
-            if tmp[i] == 'a' or tmp[i] == 'e' or tmp[i] == 'i' or tmp[i] == 'o' or tmp[i] == 'u':
+        for s in rlt:
+            if s == 'a' or s == 'e' or s == 'i' or s == 'o' or s == 'u':
                 cnt1 += 1
             else:
                 cnt2 += 1
         if cnt1 >= 1 and cnt2 >= 2:
-            rlt = ''.join(map(str,tmp))
             print(rlt)
         return
 
     for i in range(idx,C):
-        tmp.append(lst[i])
-        dfs(n+1,i+1)
-        tmp.pop()
-
-dfs(0,0)
+        dfs(n+1,i+1,rlt+lst[i])
+dfs(0,0,'')
