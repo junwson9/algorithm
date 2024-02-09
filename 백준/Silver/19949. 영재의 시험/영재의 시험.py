cd3cad0a6ca207a669e052e70b030af1f68b89b2
JW <119388872+junwson9@users.noreply.github.com>
2024-02-09 21:42:43 +0900

    [Silver II] Title: 영재의 시험, Time: 2036 ms, Memory: 116232 KB -BaekjoonHub

---
+++ 백준/Silver/19949. 영재의 시험/영재의 시험.py
@@ -0,0 +1,26 @@
+import sys
+input = sys.stdin.readline
+lst = list(map(int,input().split()))
+ans = 0
+def dfs(n,tmp):
+    global ans
+    if len(tmp) >= 3:
+        for i in range(len(tmp)-2):
+            if tmp[i] == tmp[i+1] and tmp[i+1] == tmp[i+2]:
+                return
+
+    if n == 10:
+        cnt = 0
+        for i in range(10):
+            if lst[i] == tmp[i]:
+                cnt += 1
+        if cnt >= 5:
+            ans += 1
+        return
+
+    for i in range(1,6):
+        dfs(n+1,tmp+[i])
+
+
+dfs(0,[])
+print(ans)
