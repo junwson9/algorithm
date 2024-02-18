ce22f3bf83963489846afbae5210a7fdaa7ce09d
JW <119388872+junwson9@users.noreply.github.com>
2024-02-18 13:56:57 +0900

    [Silver III] Title: 2×n 타일링, Time: 112 ms, Memory: 109444 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
@@ -1,8 +1,20 @@
-n = int(input())
+import sys
+input = sys.stdin.readline
+N = int(input())
+ans = 0
+dp = [-1]*1001
+def dfs(n):
+    global ans
+    if n > N:
+        return 0
 
-dp = [0]*1001
-dp[1] = 1
-dp[2] = 2
-for i in range(3,n+1):
-    dp[i] = (dp[i-1]+dp[i-2]) % 10007
-print(dp[n])
+    if n == N:
+        return 1
+
+    if dp[n] != -1:
+        return dp[n]
+    ans = (dfs(n+1)+dfs(n+2)) % 10007
+    dp[n] = ans
+    return dp[n]
+dfs(0)
+print(dp[0])
