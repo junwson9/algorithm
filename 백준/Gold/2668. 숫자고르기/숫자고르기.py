e996623b68e92c2ccb0a7df7d3ebf728c26ffa26
JW <119388872+junwson9@users.noreply.github.com>
2024-03-08 10:28:27 +0900

    [Gold V] Title: 숫자고르기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/2668. 숫자고르기/숫자고르기.py
+++ 백준/Gold/2668. 숫자고르기/숫자고르기.py
@@ -1,26 +1,34 @@
-def dfs(n,stack):
-    for v in adjL[n]:
-        if visited[v] == 0:
-            visited[v] = 1
-            dfs(v, stack+[v])
-            visited[v] = 0
-        else:
-            while stack:
-                a = stack.pop()
-                ans.add(a)
-                if v == a:
-                    break
-            return
-
-
-ans = set()
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10**7)
 N = int(input())
 adjL = [[] for _ in range(N+1)]
-visited = [0]*(N+1)
+
 for i in range(1,N+1):
-    adjL[int(input())].append(i)
-for n in range(1,N+1):
-    dfs(n,[])
-ans = sorted(list(ans))
-print(len(ans))
-print(*ans, sep='\n')
+    j = int(input())
+    adjL[j].append(i)
+rlt = set()
+def dfs(n,tmp):
+    for i in adjL[n]:
+        if not v[i]:
+            v[i] = True
+            dfs(i,tmp+[i])
+        else:
+            while tmp:
+                a = tmp.pop()
+                rlt.add(a)
+                if i == a:
+                    break
+    return
+
+for i in range(1,N+1):
+    v = [False]*(N+1)
+    dfs(i,[])
+rlt = sorted(list(rlt))
+print(len(rlt))
+for n in rlt:
+    print(n)
+
+
+
+
