1d6fa4cf9822c45b50bffe8d5b4f0342327a3ea7
JW <119388872+junwson9@users.noreply.github.com>
2024-03-09 16:39:36 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 688 ms, Memory: 65048 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -1,24 +1,27 @@
-def bfs(s):
-    q = [s]
-    visited[s] = 1
-    while q:
-        t = q.pop(0)
-        for i in adjL[t]:
-            if visited[i] == 0:
-                q.append(i)
-                visited[i] = 1
-
-
-N, M = map(int, input().split())
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10**7)
+N,M = map(int,input().split())
 adjL = [[] for _ in range(N+1)]
-visited = [0]*(N+1)
+for _ in range(M):
+    x,y = map(int, input().split())
+    adjL[x].append(y)
+    adjL[y].append(x)
+v = [False]*(N+1)
+def dfs(n):
+    v[n] = True
+    for i in adjL[n]:
+        if v[i]:
+            continue
+        dfs(i)
 cnt = 0
-for i in range(M):
-    s, e = map(int, input().split())
-    adjL[s].append(e)
-    adjL[e].append(s)
 for i in range(1,N+1):
-    if visited[i] == 0:
-        bfs(i)
-        cnt += 1
+    if v[i]:
+        continue
+    dfs(i)
+    cnt += 1
 print(cnt)
+
+
+
+
