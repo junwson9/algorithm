b417d4acf7b4d57b406f13a2b84d280a2a3ed945
JW <119388872+junwson9@users.noreply.github.com>
2024-10-23 14:10:46 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 320 ms, Memory: 172776 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -1,27 +1,25 @@
 import sys
+
 input = sys.stdin.readline
-sys.setrecursionlimit(10**7)
 N,M = map(int,input().split())
 adjL = [[] for _ in range(N+1)]
 for _ in range(M):
-    x,y = map(int, input().split())
-    adjL[x].append(y)
-    adjL[y].append(x)
-v = [False]*(N+1)
+    s,e = map(int, input().split())
+    adjL[s].append(e)
+    adjL[e].append(s)
+
+
+ans = 0
+visited = [0]*(N+1)
 def dfs(n):
-    v[n] = True
-    for i in adjL[n]:
-        if v[i]:
-            continue
-        dfs(i)
-cnt = 0
+    visited[n] = 1
+    for v in adjL[n]:
+        if visited[v] == 0:
+            dfs(v)
+
 for i in range(1,N+1):
-    if v[i]:
+    if visited[i] == 1:
         continue
     dfs(i)
-    cnt += 1
-print(cnt)
-
-
-
-
+    ans += 1
+print(ans)
