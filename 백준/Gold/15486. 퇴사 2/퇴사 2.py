e2c2547225bcd8f47cb6121dc97dbea5e9cf481e
JW <119388872+junwson9@users.noreply.github.com>
2025-02-12 09:59:30 +0900

    [Gold V] Title: 퇴사 2, Time: 916 ms, Memory: 287816 KB -BaekjoonHub

--- 백준/Gold/15486. 퇴사 2/퇴사 2.py
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.py
@@ -1,9 +1,30 @@
 import sys
+
 input = sys.stdin.readline
 N = int(input())
 arr = [list(map(int,input().split())) for _ in range(N)]
-dp = [-9999]*(N+53)
+
+dp = [-99999]*(N+100)
+
 dp[N] = 0
+
 for i in range(N-1,-1,-1):
     dp[i] = max(dp[i+1],dp[i+arr[i][0]]+arr[i][1])
+
 print(dp[0])
+
+
+# def dfs(n):
+#     if n > N:
+#         return -12375891236754367
+#
+#     if n == N:
+#         return 0
+#
+#     if dp[n] != -1:
+#         return dp[n]
+#
+#     dp[n] = max(dfs(n+1),dfs(n+arr[n][0])+arr[n][1])
+#     return dp[n]
+#
+# dfs(0)
