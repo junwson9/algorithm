b6f7dbc0ba268aef15c5be8fd0b36a59fc81d1c7
JW <119388872+junwson9@users.noreply.github.com>
2025-02-13 11:36:51 +0900

    [Silver II] Title: 제곱수의 합, Time: 268 ms, Memory: 112304 KB -BaekjoonHub

--- 백준/Silver/1699. 제곱수의 합/제곱수의 합.py
+++ 백준/Silver/1699. 제곱수의 합/제곱수의 합.py
@@ -1,11 +1,40 @@
 import sys
+import math
 input = sys.stdin.readline
 N = int(input())
-dp = [9999999999999]*(N+1)
-dp[N] = 0
-for total in range(N-1,-1,-1):
-    for i in range(int(N**0.5),0,-1):
-        if total+i**2 <= N:
-            dp[total] = min(dp[total], dp[total+i**2]+1)
-print(dp[0])
+lmt = int(math.sqrt(N))
 
+dp = [float('inf')] * (N+1)
+dp[0]=0
+
+
+for n in range(N+1):
+    for i in range(1,lmt+1):
+        if n+i**2 > N:
+            break
+        dp[n+i**2] = min(dp[n+i**2],dp[n]+1)
+
+print(dp[N])
+
+# dp = [-1]*100001
+#
+# def dfs(n):
+#     if n > N:
+#         return 12356123476
+#
+#     if n == N:
+#         return 0
+#
+#     if dp[n] != -1:
+#         return dp[n]
+#
+#     ans = 1e9
+#
+#     for i in range(1,lmt+1):
+#         ans = min(dfs(n+i**2)+1,ans)
+#     dp[n] = ans
+#     return dp[n]
+#
+#
+# dfs(0)
+# print(dp[0])
