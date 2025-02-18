cd9adb206dfcd36401536dc19188471bd07b4f34
JW <119388872+junwson9@users.noreply.github.com>
2025-02-18 20:47:49 +0900

    [Silver V] Title: 숫자 카드, Time: 492 ms, Memory: 218948 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.py
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -4,33 +4,24 @@ input = sys.stdin.readline
 
 N = int(input())
 lst = list(map(int,input().split()))
+lst.sort()
 M = int(input())
 find = list(map(int,input().split()))
 
-lst.sort()
 
-answer = []
-
-def binary_search (s,e,i):
+for num in find:
+    s = 0
+    e = N-1
+    ans = 0
     while s<=e:
         mid = (s+e)//2
-        if lst[mid] < find[i]:
+        if lst[mid] < num:
             s = mid+1
-        elif lst[mid] > find[i]:
+        elif lst[mid] > num:
             e = mid-1
         else:
-            return 1
-    return 0
-
-
-
-for i in range(len(find)):
-    s = 0
-    e = len(lst)-1
-    answer.append(binary_search(s,e,i))
-
-
-print(*answer)
-
+            ans = 1
+            break
+    print(ans,end=" ")
 
 
