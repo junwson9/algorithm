2883d31b2d0c804022ee52981b96002509056192
JW <119388872+junwson9@users.noreply.github.com>
2025-02-24 17:14:57 +0900

    [Silver III] Title: 바이러스, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,25 +1,29 @@
 import sys
 
 input = sys.stdin.readline
+
 N = int(input())
 M = int(input())
 adjL = [[] for _ in range(N+1)]
+
 for _ in range(M):
-    s,e = map(int ,input().split())
+    s,e = map(int,input().split())
     adjL[s].append(e)
     adjL[e].append(s)
 
 visited = [0]*(N+1)
-visited[1] = 1
-cnt = 0
+
 def dfs(n):
-    global cnt
+
     for v in adjL[n]:
-        if visited[v] == 0:
+        if not visited[v]:
             visited[v] = 1
             dfs(v)
-            cnt += 1
-
-
 dfs(1)
+cnt = 0
+for i in range(2,N+1):
+    if visited[i]:
+        cnt += 1
+
+
 print(cnt)
