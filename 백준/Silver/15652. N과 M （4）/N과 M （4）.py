a20ca584aee5b611661da3d7fea28ea0b8559fea
JW <119388872+junwson9@users.noreply.github.com>
2025-02-24 20:32:00 +0900

    [Silver III] Title: N과 M (4), Time: 136 ms, Memory: 112112 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.py
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -1,16 +1,15 @@
 import sys
+
 input = sys.stdin.readline
-N,M = map(int, input().split())
-lst = []
-def dfs(n,idx):
+
+N,M = map(int,input().split())
+
+def dfs(n,idx,tmp):
     if n == M:
-        print(*lst)
+        print(*tmp)
         return
+
     for i in range(idx,N+1):
-            lst.append(i)
-            dfs(n+1,i)
-            lst.pop()
-dfs(0,1)
+        dfs(n+1,i,tmp+[i])
 
-
-
+dfs(0,1,[])
