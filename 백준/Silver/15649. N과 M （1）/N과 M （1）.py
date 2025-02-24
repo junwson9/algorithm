696733c6ccf73b5cde47e8e5fa24e2b3486316b7
JW <119388872+junwson9@users.noreply.github.com>
2025-02-24 20:23:27 +0900

    [Silver III] Title: N과 M (1), Time: 164 ms, Memory: 114144 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,19 +1,19 @@
-def bt(n):
+import sys
+
+input = sys.stdin.readline
+
+N,M = map(int,input().split())
+v = [0]*(N+1)
+
+def dfs(n,tmp):
     if n == M:
-        print(' '.join(map(str, stk)))
-        return  #종료 조건 - 들어갔는데 M과 깊이가 같아지면 한칸띄우고 출력
-    for i in range(1, N+1):
-        if visit[i] == 0:
-            visit[i] = 1    #방문한곳 표시
-            stk.append(i) #1부터 방문안했으면  추가
-            bt(n+1) #다음 깊이로 드감
-            visit[i] = 0    #올라갈때 visit초기화 해주면서 올라감
-            stk.pop()   #위 단계로 올라올때 pop임
+        print(*tmp)
+        return
 
+    for i in range(1,N+1):
+        if v[i] == 0:
+            v[i] = 1
+            dfs(n+1,tmp+[i])
+            v[i] = 0
 
-
-N, M = map(int, input().split())
-# 1부터 N까지 숫자들로 M길이 수열 출력하는 단순한문제
-visit = [0]*(N+1)
-stk = []
-bt(0)
+dfs(0,[])
