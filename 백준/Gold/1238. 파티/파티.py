1dcc1da2bdaecaf2bd807c84ce1cb4008e9e859a
JW <119388872+junwson9@users.noreply.github.com>
2025-02-25 16:42:42 +0900

    [Gold III] Title: 파티, Time: 1252 ms, Memory: 122104 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.py
+++ 백준/Gold/1238. 파티/파티.py
@@ -1,36 +1,41 @@
-import heapq
 import sys
-from collections import deque
+import heapq
+
 input = sys.stdin.readline
-N,M,X = map(int, input().split())
-adjL = [[] for _ in range(N+1)]
-for _ in range(M):
-    s,e,cost = map(int, input().split())
-    adjL[s].append((e,cost))
+n,m,x = map(int,input().split())
+adjL = [[] for _ in range(n+1)]
+
+for _ in range(m):
+    s,e,w = map(int,input().split())
+    adjL[s].append([e,w])
 
 def dijkstra(start):
     q = []
     heapq.heappush(q,(0,start))
-    dist = [int(1e9)]*(N+1)
+    dist = [int(1e9)]*(n+1)
     dist[start] = 0
     while q:
-        x_dist, x = heapq.heappop(q)
-        if dist[x] < x_dist:
+        dist_x, x = heapq.heappop(q)
+        if dist_x != dist[x]:
             continue
-        for nxt, cost in adjL[x]:
-            if dist[nxt] > x_dist + cost:
-                dist[nxt] = x_dist + cost
-                heapq.heappush(q, (dist[nxt], nxt))
+        for v,weight in adjL[x]:
+            if dist[v] > dist[x]+weight:
+                dist[v] = dist[x]+weight
+                heapq.heappush(q,(dist[v],v))
     return dist
 
 ans = 0
-for i in range(1,N+1):
-    if i == X:
+for i in range(1,n+1):
+    if i == x:
         continue
     go = dijkstra(i)
-    back = dijkstra(X)
-    ans = max(ans,go[X]+back[i])
-
+    back = dijkstra(x)
+    ans = max(ans,go[x]+back[i])
 
 print(ans)
+
+
+
+
+
 
