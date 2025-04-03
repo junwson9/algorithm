6e9d4d1315fcb98d1475cbf397491d4b1857a02d
JW <119388872+junwson9@users.noreply.github.com>
2025-04-03 18:59:29 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 308 ms, Memory: 311216 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
@@ -1,26 +1,22 @@
-from collections import deque
 import sys
 input = sys.stdin.readline
-N = int(input())
-adjL = [[] for _ in range(N+1)]
-v = [0]*(N+1)
-answer = [0]*(N+1)
-for _ in range(N-1):
+sys.setrecursionlimit(10**5)
+
+n = int(input())
+adjL = [[] for _ in range(n+1)]
+parent = [0]*(n+1)
+for _ in range(n-1):
     s,e = map(int,input().split())
     adjL[s].append(e)
     adjL[e].append(s)
-def bfs(adjL,v,node):
-    q = deque()
-    q.append(node)
-    v[node] = 1
-    while q:
-        t = q.popleft()
-        for i in adjL[t]:
-            if v[i] == 0:
-                q.append(i)
-                answer[i] = t
-                v[i] = 1
-bfs(adjL,v,1)
 
-for i in range(2,N+1):
-    print(answer[i])
+def dfs(cur,prev):
+    for nxt in adjL[cur]:
+        if nxt == prev:
+            continue
+        parent[nxt] = cur
+        dfs(nxt,cur)
+
+dfs(1,0)
+for i in range(2,n+1):
+    print(parent[i])
