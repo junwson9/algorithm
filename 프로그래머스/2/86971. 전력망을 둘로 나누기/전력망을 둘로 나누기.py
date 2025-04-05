53f822135f351626f8e5cd2e3332e8e9bb41f5e6
JW <119388872+junwson9@users.noreply.github.com>
2025-04-05 11:01:45 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 2.55 ms, Memory: 9.29 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -1,38 +1,43 @@
 from collections import deque
-def bfs(adjL,v,start):
-    q = deque()
-    q.append(start)
-    v[start] = 1
-    cnt = 0
-    while q:
-        t = q.popleft()
-        cnt += 1
-        for i in adjL[t]:
-            if v[i] == 0:
-                v[i] = 1
-                q.append(i)
-    return cnt
-    
 
 def solution(n, wires):
-    mn = n
-    for i in range(len(wires)):
-        v = [0]*(n+1)
-        adjL = [[] for _ in range(n+1)]
-        tmp = wires.copy()
-        tmp.pop(i)
-        start = 0
-        for s,e in tmp:
-            adjL[s].append(e)
-            adjL[e].append(s)
-        for s in range(len(adjL)):
-            if adjL[s] != []:
-                start = s
-                break
-        cnts = bfs(adjL,v,start)
-        other_cnts = n-cnts
-        if abs(cnts - other_cnts) < mn:
-            mn = abs(cnts-other_cnts)
+    tree = [[] for _ in range(n+1)]
+    
+    for i in range(n-1):
+        a,b = wires[i]
+        tree[a].append(b)
+        tree[b].append(a)
         
+    
+    def bfs(start):
+        q = deque([start])
+        visited = [0]*(n+1)
+        visited[start] = 1
+        cnt = 1
+        while q:
+            cur = q.popleft()
+            for nxt in tree[cur]:
+                if visited[nxt] == 0:
+                    q.append(nxt)
+                    visited[nxt] = 1
+                    cnt += 1
+        return cnt
+    
+    answer = n
+    
+    for i in range(n-1):
+        a,b = wires[i]
+        tree[a].remove(b)
+        tree[b].remove(a)
+        answer = min(answer,abs(bfs(a)-bfs(b)))
+        tree[a].append(b)
+        tree[b].append(a)
+                    
+                
+            
+
+    
+
         
-    return mn
+
+    return answer
