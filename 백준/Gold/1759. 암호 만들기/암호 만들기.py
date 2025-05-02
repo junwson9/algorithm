1d2fea6c3ec75c10a0d17c53076040718905814f
JW <119388872+junwson9@users.noreply.github.com>
2025-05-02 10:43:29 +0900

    [Gold V] Title: 암호 만들기, Time: 112 ms, Memory: 237664 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.py
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.py
@@ -1,21 +1,29 @@
 import sys
 input = sys.stdin.readline
-L,C = map(int, input().split())
+sys.setrecursionlimit(10**5)
+L,C = map(int,input().split())
 lst = list(input().split())
 lst.sort()
-def dfs(n,idx,rlt):
+
+
+def dfs(n,idx,tmp):
     if n == L:
         cnt1 = 0
         cnt2 = 0
-        for s in rlt:
-            if s == 'a' or s == 'e' or s == 'i' or s == 'o' or s == 'u':
+        for s in tmp:
+            if s == 'a' or s =='e' or s == 'i' or s == 'o' or s =='u':
                 cnt1 += 1
             else:
                 cnt2 += 1
         if cnt1 >= 1 and cnt2 >= 2:
-            print(rlt)
+            print(tmp)
         return
 
     for i in range(idx,C):
-        dfs(n+1,i+1,rlt+lst[i])
+        dfs(n+1,i+1,tmp+lst[i])
+
 dfs(0,0,'')
+
+
+
+
