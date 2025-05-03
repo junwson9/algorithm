6e93a18d1c69418ac0452858efefef035483207c
JW <119388872+junwson9@users.noreply.github.com>
2025-05-03 16:41:21 +0900

    [Gold IV] Title: 두 로봇, Time: 448 ms, Memory: 360784 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/15971. 두 로봇/두 로봇.py
+++ 백준/Gold/15971. 두 로봇/두 로봇.py
@@ -1,25 +1,24 @@
 import sys
 input = sys.stdin.readline
-sys.setrecursionlimit(10**7)
-N,s1,s2 = map(int,input().split())
-adjL = [[] for _ in range(N+1)]
-for i in range(N-1):
-    s,e,z = map(int, input().split())
-    adjL[s].append([e,z])
-    adjL[e].append([s,z])
+sys.setrecursionlimit(10**5)
+N,a,b = map(int,input().split())
+graph = [[] for _ in range(N+1)]
+for _ in range(N-1):
+    u,v,cost = map(int,input().split())
+    graph[u].append([v,cost])
+    graph[v].append([u,cost])
 
 v = [False]*(N+1)
-rlt = []
-def dfs(n,tmp,mx):
-    v[n] = True
-    if n == s2:
-        print(tmp-mx)
-    for i,z in adjL[n]:
-        if v[i]:
-            continue
-        dfs(i,tmp+z,max(z,mx))
 
-if s1 == s2:
+def dfs(cur,tmp,mx):
+    v[cur] = True
+    if cur == b:
+        print(tmp-mx)
+    for nxt,cost in graph[cur]:
+        if not v[nxt]:
+            dfs(nxt,tmp+cost,max(cost,mx))
+
+if a == b:
     print(0)
 else:
-    dfs(s1,0,0)
+    dfs(a,0,0)
