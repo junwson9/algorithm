c1f067bc10ea5a30a0551babfb13e12bbabaed94
JW <119388872+junwson9@users.noreply.github.com>
2025-05-03 16:09:13 +0900

    [Silver II] Title: 촌수계산, Time: 88 ms, Memory: 236388 KB -BaekjoonHub

--- 백준/Silver/2644. 촌수계산/촌수계산.py
+++ 백준/Silver/2644. 촌수계산/촌수계산.py
@@ -1,33 +1,27 @@
 import sys
 input = sys.stdin.readline
-sys.setrecursionlimit(10**7)
+sys.setrecursionlimit(10**5)
 N = int(input())
-s,e = map(int, input().split())
+u,v = map(int,input().split())
 M = int(input())
-adjL = [[] for _ in range(N+1)]
+graph = [[] for _ in range(N+1)]
 for _ in range(M):
-    x,y = map(int, input().split())
-    adjL[x].append(y)
-    adjL[y].append(x)
-v = [False]*(N+1)
+    x,y = map(int,input().split())
+    graph[x].append(y)
+    graph[y].append(x)
+
+visited = [False]*(N+1)
 ans = []
-def dfs(n,cnt):
-    if n == e:
-        ans.append(cnt)
-        return
-    v[n] = True
-    for i in adjL[n]:
-        if v[i]:
-            continue
-        dfs(i,cnt+1)
+def dfs(cur,cnt):
+    visited[cur] = True
+    if cur == v:
+        return cnt
 
+    for nxt in graph[cur]:
+        if not visited[nxt]:
+            rlt = dfs(nxt,cnt+1)
+            if rlt != -1:
+                return rlt
+    return -1
 
-dfs(s,0)
-if len(ans):
-    print(*ans)
-else:
-    print(-1)
-
-
-
-
+print(dfs(u,0))
