de48f7a9ae1e8d052539374c6b39feef31a0d879
JW <119388872+junwson9@users.noreply.github.com>
2025-05-07 17:28:28 +0900

    [Silver I] Title: 미로 탐색, Time: 116 ms, Memory: 114360 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,31 +1,22 @@
 import sys
 from collections import deque
-
 input = sys.stdin.readline
+n,m = map(int,input().split())
+arr = [list(map(int,input().strip())) for _ in range(n)]
+visited = [[0]*m for _ in range(n)]
+
 def bfs(si,sj):
+    visited[si][sj] = 1
     q = deque()
     q.append([si,sj])
     while q:
         ci,cj = q.popleft()
-        if ci == N-1 and cj == M-1:
-            return
-        for di,dj in ((1,0),(0,1),(-1,0),(0,-1)):
+        for di,dj in((1,0),(0,1),(-1,0),(0,-1)):
             ni,nj = ci+di,cj+dj
-            if 0 > ni or ni >= N or 0 > nj or nj >= M:
-                continue
-            if arr[ni][nj] == 0:
-                continue
-            if v[ni][nj] < v[ci][cj]+1:
-                q.append([ni,nj])
-                v[ni][nj] = v[ci][cj]+1
+            if 0<=ni<n and 0<=nj<m:
+                if arr[ni][nj] == 1 and visited[ni][nj] == 0:
+                    visited[ni][nj] = visited[ci][cj]+1
+                    q.append([ni,nj])
 
-
-
-
-N,M = map(int,input().split())
-arr = [list(map(int, input().strip())) for _ in range(N)]
-v = [[0]*M for _ in range(N)]
-
-v[0][0] = 1
 bfs(0,0)
-print(v[N-1][M-1])
+print(visited[n-1][m-1])
