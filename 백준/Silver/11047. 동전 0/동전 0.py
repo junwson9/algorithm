01b0961a13a5a0cdf868384a7d914ab3bd9ba3e3
JW <119388872+junwson9@users.noreply.github.com>
2025-05-07 18:21:56 +0900

    [Silver IV] Title: 동전 0, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.py
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -1,15 +1,14 @@
-N, K = map(int, input().split())
-lst = [int(input()) for _ in range(N)]
-lst.reverse()
-i = 0
-ans = 0
-while True:
-    if K == 0:
-        break
-    if lst[i] > K:
-        i += 1
-    else:
-        K -= lst[i]
-        ans += 1
-print(ans)
+import sys
 
+input = sys.stdin.readline
+N,K = map(int,input().split())
+lst = []
+for _ in range(N):
+    lst.append(int(input()))
+lst.sort(reverse=True)
+ans = 0
+for i in range(N):
+    if K//lst[i]:
+        ans += K//lst[i]
+        K = K%lst[i]
+print(ans)
