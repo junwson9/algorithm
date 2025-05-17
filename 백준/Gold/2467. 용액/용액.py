e1911e44d22e57d3a3facf3a9efd169472e530fc
JW <119388872+junwson9@users.noreply.github.com>
2025-05-17 17:56:12 +0900

    [Gold V] Title: 용액, Time: 116 ms, Memory: 126004 KB -BaekjoonHub

--- 백준/Gold/2467. 용액/용액.py
+++ 백준/Gold/2467. 용액/용액.py
@@ -1,23 +1,29 @@
 import sys
+
 input = sys.stdin.readline
-n = int(input())
+N = int(input())
 lst = list(map(int,input().split()))
 s = 0
-e = n-1
-ans = abs(lst[s] + lst[e])
-left = lst[s]
-right = lst[e]
-while s < e:
-    tmp = lst[s]+lst[e]
-    if abs(tmp) < ans:
-        left = lst[s]
-        right = lst[e]
-        ans = abs(tmp)
-
+e = N-1
+ans = abs(lst[s]+lst[e])
+l = lst[s]
+r = lst[e]
+while s<e:
+    sm = lst[s]+lst[e]
+    if abs(sm) < ans:
+        ans = abs(sm)
+        l = lst[s]
+        r = lst[e]
+        
         if ans == 0:
             break
-    if tmp < 0:
+    
+    if sm < 0:
         s += 1
     else:
         e -= 1
-print(left, right)
+print(l,r)
+
+
+
+
