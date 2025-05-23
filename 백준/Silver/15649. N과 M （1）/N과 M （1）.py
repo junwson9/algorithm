eb1aee34c798f2b0580bb43f40bac83a783b6210
JW <119388872+junwson9@users.noreply.github.com>
2025-05-23 18:15:28 +0900

    [Silver III] Title: N과 M (1), Time: 156 ms, Memory: 114052 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,19 +1,21 @@
 import sys
 
 input = sys.stdin.readline
-
 N,M = map(int,input().split())
+tmp = []
 v = [0]*(N+1)
-
-def dfs(n,tmp):
+def dfs(n):
     if n == M:
         print(*tmp)
         return
 
     for i in range(1,N+1):
-        if v[i] == 0:
+        if not v[i]:
             v[i] = 1
-            dfs(n+1,tmp+[i])
+            tmp.append(i)
+            dfs(n+1)
             v[i] = 0
+            tmp.pop()
+dfs(0)
 
-dfs(0,[])
+
