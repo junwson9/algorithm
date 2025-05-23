ea854ca9f57c46990b99cabe58e76fe2a8a0fccf
JW <119388872+junwson9@users.noreply.github.com>
2025-05-23 18:26:57 +0900

    [Silver III] Title: N과 M (6), Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/15655. N과 M （6）/N과 M （6）.py
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.py
@@ -1,16 +1,21 @@
 import sys
 
 input = sys.stdin.readline
-
 N,M = map(int,input().split())
-lst = sorted(list(map(int,input().split())))
+lst = list(map(int,input().split()))
+lst.sort()
+tmp = []
 
-def dfs(n,idx,tmp):
+def dfs(n,idx):
     if n == M:
         print(*tmp)
         return
 
-    for i in range(idx,N):
-        dfs(n+1,i+1,tmp+[lst[i]])
+    for i in range(idx,len(lst)):
+        tmp.append(lst[i])
+        dfs(n+1,i+1)
+        tmp.pop()
 
-dfs(0,0,[])
+dfs(0,0)
+
+
